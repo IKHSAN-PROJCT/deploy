7979c68ea34a2e78281c52ffe00d782d5cd5088d
SANZPROJECT <151718020+IKHSAN-PROJCT@users.noreply.github.com>
2026-02-07 20:07:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-let state = {
-  command: "NONE",
-  time: Date.now()
+let LAST_COMMAND = {
+  id: 0,
+  command: "NONE"
 };
 
 export default {
   async fetch(req) {
-
     if (req.method === "POST") {
       const body = await req.json();
-      const msg = (body.message || "").toLowerCase();
+      LAST_COMMAND = {
+        id: Date.now(),
+        command: body.message
+      };
+      return Response.json({ status: "OK" });
+    }
 
-      if (msg.includes("nyalakan")) {
-        state.command = "FLASH_ON";
-      } else if (msg.includes("matikan")) {
-        state.command = "FLASH_OFF";
-      }
+    if (req.method === "GET") {
+      return Response.json(LAST_COMMAND);
+    }
 
-      state.time = Date.now();
-
-      return new Response(JSON.stringify(state), {
+    return new Response("Not Allowed", { status: 405 });
+  }
+};SON.stringify(state), {
         headers: { "Content-Type": "application/json" }
       });
     }
